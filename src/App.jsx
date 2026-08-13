4aee19454e383b68840e4ce59143615726269e53
omobolarin1989 <omobolarin1989@gmail.com>
2026-08-13 20:26:41 +0100

    logo image path correction

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <main className=' flex lg:flex-row flex-col justify-center items-center gap-y-3  lg:py-5 py-[30px] px-[28px] bg-primary lg:bg-[url("/public/images/bg-desktop.svg")] bg-[url("/public/images/bg-mobile.svg")] lg:bg-center lg:bg-contain lg:bg-no-repeat bg-no-repeat  lg:h-lvh w-full border border-var(--primary) text-[#FFFFFF] gap-y-4 '>
 
        <section className='logo pb-5'>
-        <img src="/public/images/logo.svg" alt="logo" className='lg:w-[50%] w-[40%]'  / >
+        <img src="/images/logo.svg" alt="logo" className='lg:w-[50%] w-[40%]'  / >
       </section>
     
     </main>
