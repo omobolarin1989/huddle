87d1f8953bcfdbbee377bc66aee0c3167e63cc77
omobolarin1989 <omobolarin1989@gmail.com>
2026-08-14 16:16:37 +0100

    page paragraph and image path correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
- 
-
   return (
-    <div className=''>
-    <main className=' flex lg:flex-row flex-col justify-center items-center gap-y-3  lg:py-5 py-[30px] px-[28px] bg-primary lg:bg-[url("/public/images/bg-desktop.svg")] bg-[url("/public/images/bg-mobile.svg")] lg:bg-center lg:bg-contain lg:bg-no-repeat bg-no-repeat  lg:h-lvh w-full border border-var(--primary) text-[#FFFFFF] gap-y-4 '>
+    <div className="">
+      <main className=' flex lg:flex-row flex-col justify-center items-center gap-y-3  lg:py-5 py-[30px] px-[28px] bg-primary lg:bg-[url("/public/images/bg-desktop.svg")] bg-[url("/public/images/bg-mobile.svg")] lg:bg-center lg:bg-contain lg:bg-no-repeat bg-no-repeat  lg:h-lvh w-full border border-var(--primary) text-[#FFFFFF] gap-y-4 '>
+        <section className="logo pb-5">
+          <img
+            src="/images/logo.svg"
+            alt="logo"
+            className="lg:w-[50%] w-[40%]"
+          />
+        </section>
 
-       <section className='logo pb-5'>
-        <img src="/images/logo.svg" alt="logo" className='lg:w-[50%] w-[40%]'  / >
-      </section>
+        <section className="huddle-page">
+          <section className="huddle-image">
+            <img
+              src="/images/illustration-mockups.svg"
+              className="lg:w-20% w-screen"
+              alt="illustration image"
+            />
+          </section>
 
-      <section className='huddle-page'>
-             <section className="huddle-image">
-        <img src="/public/images/illustration-mockups.svg" className='lg:w-20% w-screen' alt="illustration image"/>
-      </section>
+          <section className="huddle-text">
+            <div className="heading">
+              <h1 className=" text-[clamp(24px,5vw,32px)] font-poppins font-[700] text-center font-poppins">
+                Build The Community Your Fans Will Love
+              </h1>
+            </div>
 
-      <section className='huddle-text'>
-         <div className='heading'>
-      <h1 className=' text-[clamp(24px,5vw,32px)] font-poppins font-[700] text-center font-poppins'>Build The Community Your Fans Will Love</h1>
+            <div className="paragraph max-w-screen flex justify-center pb-2">
+              <p className="text-[clamp(15px,4vw,19px)]  text-center font-opensans">
+                Huddle re-imagines the way we build communities. You have a
+                voice, but so does your audience. Create connections with your
+                users as you engage in genuine discussion.{" "}
+              </p>
+            </div>
+          </section>
+        </section>
+      </main>
     </div>
-      </section>
-      </section>
-    
-    </main>
-    </div>
-  )
+  );
 }
 
-  export default App
+export default App;
