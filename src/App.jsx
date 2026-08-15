45a933bb0caead6f5149ba43169c8a9185788acf
omobolarin1989 <omobolarin1989@gmail.com>
2026-08-15 02:13:09 +0100

    heading paragraph CTA and social development

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
+import "./App.css";
 import { GrFacebookOption } from "react-icons/gr";
 import { TbBrandTwitterFilled } from "react-icons/tb";
 import { SlSocialInstagram } from "react-icons/sl";
 
 function App() {
   return (
-
-    
     <div>
-      <main className=' flex flex-col items-center gap-y-3  lg:py-[30px] py-[30px] lg:px-[40px] px-[28px] bg-primary lg:bg-[url("/public/images/bg-desktop.svg")] bg-[url("/public/images/bg-mobile.svg")] lg:bg-center lg:bg-contain lg:bg-no-repeat bg-no-repeat  lg:h-lvh h-lvh w-full text-[#FFFFFF] gap-y-4 '>
+      <main className=' flex flex-col items-center gap-y-3  lg:py-[30px] py-[30px] lg:px-[40px] px-[28px] bg-primary lg:bg-[url("/public/images/bg-desktop.svg")] bg-[url("/public/images/bg-mobile.svg")] lg:bg-center lg:bg-contain lg:bg-no-repeat bg-no-repeat  lg:h-vh h-100vh w-full text-[#FFFFFF] gap-y-4 '>
         <section className="logo lg:pb-[30px] pb-5 lg:w-[100%]">
           <img
             src="/images/logo.svg"
@@ -26,53 +24,66 @@ function App() {
             />
           </section>
 
-          <section className="huddle-text lg:w-[40%] flex flex-col lg:mt-[60px]">
-            <div className="heading">
+          <section className="huddle-text lg:w-[40%] lg:justify-between flex flex-col lg:mt-[60px]">
+            <div className="header-para flex flex-col gap-y-4">
+            <div className="heading lg:leading-[310%]">
               <h1 className=" text-[clamp(24px,5vw,56px)] font-poppins font-[700] lg:text-left text-center ">
                 Build The Community Your Fans Will Love
               </h1>
             </div>
 
             <div className="paragraph max-w-screen flex justify-center pb-2">
-              <p className="lg:text-[clamp(15px,3vw,25px)]  text-[clamp(15px,4vw,19px)] lg:text-left text-center font-opensans">
+              <p className="lg:text-[clamp(15px,3vw,25px)]  text-[clamp(15px,4vw,19px)] lg:font-[50] lg:text-left text-center font-opensans">
                 Huddle re-imagines the way we build communities. You have a
                 voice, but so does your audience. Create connections with your
                 users as you engage in genuine discussion.{" "}
               </p>
             </div>
             <div className="cta-section flex justify-center lg:justify-start">
-            <button className="bg-[#FFFFFF] lg:w-[35%] w-[65%] rounded-[20px] text-primary py-2 lg:py-3  text-[14px] font-[500] hover:bg-active hover:text-[#FFFFFF] cursor-pointer">
-              Register
-            </button>
+              <button className="bg-[#FFFFFF] lg:w-[35%] w-[75%] rounded-[20px] text-primary py-2 lg:py-3 lg:text-[16px] text-[14px] font-[500] lg:font-[700] hover:bg-active hover:text-[#FFFFFF] cursor-pointer font-opensans">
+                Register
+              </button>
+            </div>
             </div>
 
-            <section className='socials w-full flex justify-center gap-3 mt-[40px]'>
+            <section className="socials w-full lg:h-auto flex justify-center lg:justify-end gap-3 mt-[40px] ">
+              <div className="hover:border hover:text-active hover:border-active border-2 p-[8px] rounded-[50%] flex justify-center items-center cursor-pointer">
+                <a href="">
+                  <GrFacebookOption className="" />
+                </a>
+              </div>
 
-  <div className='hover:border hover:text-active hover:border-active border-2 p-[8px] rounded-[50%] flex justify-center items-center cursor-pointer'>
-   <a href=""><GrFacebookOption className='' / ></a>
-   </div>
-  
-  <div className='hover:border hover:text-active hover:border-active border-2 p-[6px] rounded-[50%] flex justify-center items-center cursor-pointer'>
-   <a href=""><TbBrandTwitterFilled /></a>
-   </div>
-  
-  <div className='hover:border hover:text-active hover:border-active border-2 p-[6px] rounded-[50%] flex justify-center items-center cursor-pointer'>
-   <a href=""><SlSocialInstagram /></a>
-   </div>
-  
-</section>
+              <div className="hover:border hover:text-active hover:border-active border-2 p-[6px] rounded-[50%] flex justify-center items-center cursor-pointer">
+                <a href="">
+                  <TbBrandTwitterFilled />
+                </a>
+              </div>
 
+              <div className="hover:border hover:text-active hover:border-active border-2 p-[6px] rounded-[50%] flex justify-center items-center cursor-pointer">
+                <a href="">
+                  <SlSocialInstagram />
+                </a>
+              </div>
+            </section>
           </section>
         </section>
       </main>
 
       <footer className="attribution text-center">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" className='text-attribution-link' >Frontend Mentor</a>. 
-      Coded by <a href="#" className='text-attribution-link'>Olaniyi Olatunbosun</a>.
-    </footer>
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          className="text-attribution-link"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="#" className="text-attribution-link">
+          Olaniyi Olatunbosun
+        </a>
+        .
+      </footer>
     </div>
-
-
   );
 }
 
