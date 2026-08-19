c6a01446704680dde4a2e45b820eadc7ad05f2c4
omobolarin1989 <omobolarin1989@gmail.com>
2026-08-19 20:19:48 +0100

    font and text-sizing correction

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,33 @@ import { SlSocialInstagram } from "react-icons/sl";
 function App() {
   return (
     <div>
-      <main className="bg-[url('/images/bg-mobile.svg')] bg-primary w-full min-h-screen md:border-3 md:border-amber-600 px-[32px] py-[30px] flex flex-col bg-contain bg-no-repeat text-[#FFFFFF] md:gap-y-[40px] gap-y-4">
+      <main className="bg-[url('/images/bg-mobile.svg')] bg-primary w-full min-h-screen md:border-3 lg:border-amber-600 px-[38px] py-[30px] lg:px-[80px] flex flex-col bg-contain bg-no-repeat text-[#FFFFFF] md:gap-y-[40px] gap-y-4">
         <section className="logo w-[100%]">
-       <img src="/images/logo.svg" alt="logo" className=" w-[55%] md:w-[20%]"/>
+       <img src="/images/logo.svg" alt="logo" className=" w-[55%] md:w-[20%] lg:w-[15%]"/>
        </section>
 
-       <section className="page-content flex flex-col gap-y-5">
-          <div className="illustration-image">
+       <section className="page-content flex flex-col lg:flex-row gap-y-5 lg:w-full lg:gap-x-[50px]">
+          <div className="illustration-image lg:w-[65%]">
             <img src="/images/illustration-mockups.svg" alt="illustration image" className="w-full" />
           </div>
-          <div className="page-text">
-            <h1 className="font-poppins text-center text-[clamp(1.3rem,4vw,2.3rem)] font-[600]">Build The Community Your Fans Will Love</h1>
+          <div className="page-text lg:w-[35%] flex flex-col lg:gap-y-[30px] gap-y-4 lg:mt-[80px] border ">
+            <h1 className="font-poppins text-center lg:text-left text-[clamp(1.1rem,3vw,2.2rem)] font-[600]">Build The Community Your Fans Will Love</h1>
 
-            <p className="text-center text-[clamp(0.8rem,2vw,2.3rem)] font-opensans ">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
+            <p className="text-center text-[clamp(0.74rem,3vw,1.1rem)] lg:text-left font-opensans">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
   Create connections with your users as you engage in genuine discussion.</p>
+  <div className="w-full flex justify-center">
+  <button type="submit" className="border font-[400] text-[20px] py-[5px] lg:py-[12px] px-[20%] md:px-[120px] lg:w-[20px] flex justify-center lg:rounded-[25px] rounded-[20px] bg-[#FFFFFF] text-primary font-opensans">Register</button>
+        </div>
           </div>
+          
           
        </section>
 
        <section className="reg-button flex justify-center">
-        <button type="submit" className="border py-[7px] px-[60px] md:px-[120px] rounded-[20px] bg-[#FFFFFF] text-primary">Register</button>
+        
        </section>
 
-       <section className="social flex justify-center gap-x-2.5 md:gap-x-6 md:pt-[70px] pt-[50px]">
+       <section className="social flex justify-center lg:justify-end gap-x-2.5 md:gap-x-6 md:pt-[40px] pt-[50px]">
 
          <div className='hover:border hover:text-active hover:border-active border-1 p-[5px] md:p-[10px] md:rounded-[30px] rounded-[100%] flex justify-center items-center cursor-pointer'>
    <a href=""><GrFacebookOption className='' / ></a>
